3dffb6cb776a27f6d160280113307161fbccc818
agg22 <amber.gorzynski22@imperial.ac.uk>
2024-09-25 21:44:05 +0100

    Add example fuzzer test to the test suite

---
+++ Ghidra/Test/IntegrationTest/src/test.slow/java/ghidra/fuzz/FuzzerTest.java
@@ -0,0 +1,38 @@
+package ghidra.program.fuzz;
+
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+import ghidra.app.util.headless.AnalyzeHeadless;
+import ghidra.GhidraApplicationLayout;
+import ghidra.GhidraJarApplicationLayout;
+
+public class FuzzerTest {
+
+	@Test
+	public void fuzzTest() throws Exception {
+
+		String outputDir = "/data/work/fuzzflesh/coverage/ghidra";
+		String program = "/data/work/fuzzflesh/coverage/ghidra/prog_0.o";
+		String headlessPath = "/data/dev/fuzzflesh/src/fuzzflesh/harness/c";
+		String headlessScriptName = "DecompileHeadless.java";
+		String outputProgramName = "/data/work/fuzzflesh/coverage/ghidra/decomp_prog.c";
+
+		String[] args = {outputDir,
+			"Project", 
+			"-import",
+			program,
+			"-overwrite",
+			"-scriptPath",
+			headlessPath,
+			"-postScript",
+			headlessScriptName, 
+			outputProgramName};
+
+		GhidraApplicationLayout layout = new GhidraApplicationLayout();
+
+		new AnalyzeHeadless().launch(layout, args);
+
+	}
+}
