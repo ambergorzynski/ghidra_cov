cbd1ca69b8a0177810d0c54231011d0503dec47a
agg22 <amber.gorzynski22@imperial.ac.uk>
2024-09-30 14:21:28 +0100

    Parameterize fuzzing tests to read test locations from xml file

--- Ghidra/Test/IntegrationTest/src/test.slow/java/ghidra/fuzz/FuzzerTest.java
+++ Ghidra/Test/IntegrationTest/src/test.slow/java/ghidra/fuzz/FuzzerTest.java
@@ -1,30 +1,38 @@
-package ghidra.program.fuzz;
+package ghidra.fuzz;
 
 import static org.junit.Assert.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import ghidra.app.util.headless.AnalyzeHeadless;
 import ghidra.GhidraApplicationLayout;
 import ghidra.GhidraJarApplicationLayout;
 
+import ghidra.fuzz.FuzzerTestImplementation.FuzzerTestDataSource;
+
 public class FuzzerTest {
 
-	@Test
-	public void fuzzTest() throws Exception {
+	//@Test
+	@ParameterizedTest(name = "[{index}] {0}")
+  	@FuzzerTestDataSource("fuzzer_classes.xml")
+	public void fuzzTest(Path testFilePath) throws Exception {
 
 		String outputDir = "/data/work/fuzzflesh/coverage/ghidra";
-		String program = "/data/work/fuzzflesh/coverage/ghidra/prog_0.o";
+		String program = testFilePath.toString();
 		String headlessPath = "/data/dev/fuzzflesh/src/fuzzflesh/harness/c";
 		String headlessScriptName = "DecompileHeadless.java";
-		String outputProgramName = "/data/work/fuzzflesh/coverage/ghidra/decomp_prog.c";
+		String outputProgramName = outputDir + "/decomp_prog.c";
 
-		String[] args = {outputDir,
+		String[] args = {outputDir, 
 			"Project", 
 			"-import",
 			program,
 			"-overwrite",
-			"-scriptPath",
+			"-scriptPath", 
 			headlessPath,
 			"-postScript",
 			headlessScriptName, 
